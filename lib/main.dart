bc2e8157214fa917010b518b0c934f8b5d44a43c
Damien Dumontet <damien.dumontet@protonmail.com>
2022-07-08 23:37:52 -0300

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:naia_app/ffi.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String wordFromRust = "Placeholder";
+
+  void _changeBtnLabel() async {
+    String word = await naiaAPI.greet();
+    setState(() {
+      wordFromRust = word;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: _changeBtnLabel,
+          child: Text(wordFromRust),
+        ),
+      ),
+    );
+  }
+}
