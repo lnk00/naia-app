f2b98d47640f138a03a382b48ab12d5223b580af
Damien Dumontet <damien.dumontet@protonmail.com>
2024-01-23 20:14:30 -0300

    modify profile page design

--- app/(home)/profile.tsx
+++ app/(home)/profile.tsx
@@ -1,38 +1,76 @@
 import { useLocalSearchParams } from 'expo-router';
-import { View, Text, TouchableOpacity, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  Switch,
+} from 'react-native';
 
 export default function ModalScreen() {
   const params = useLocalSearchParams<{ fullName: string; birthday: string }>();
-  const sendMessage = () => {
-    console.log('Send message');
+  const [isEnabled, setIsEnabled] = useState(true);
+
+  const toggleSwitch = () => {
+    setIsEnabled(!isEnabled);
   };
 
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <View className="px-6 flex-1">
-        <View className="h-24 w-24 bg-main rounded-full flex items-center justify-center mt-4">
+      <View className="px-6 flex-1 items-center">
+        <View className="h-24 w-24 bg-lightGray rounded-xl flex items-center justify-center mt-4">
           <Text className="font-bold text-2xl text-dark">
             {params.fullName.split(' ')[0].charAt(0)}
             {params.fullName.split(' ')[1].charAt(0)}
           </Text>
         </View>
-        <Text className="font-heading text-5xl text-dark mt-4">
-          {params.fullName.split(' ')[0]}
-        </Text>
-        <Text className="font-heading text-5xl text-dark">
-          {params.fullName.split(' ')[1]}
+        <Text className="text-3xl font-bold text-dark mt-6">
+          {params.fullName}
         </Text>
         <View className="flex flex-row">
-          <Text className="font-bold font-heading text-4xl text-main mt-4 shrink">
+          <Text className="font-bold font-heading text-3xl text-main mt-1 shrink">
             {params.birthday}
           </Text>
         </View>
+        <View className="bg-lightGray rounded-xl p-4 mt-6 w-full">
+          <Text className="font-medium text-lg">
+            As tu pensé à une idée cadeau pour lui faire plaisir ?
+          </Text>
+          <View className="flex flex-row gap-2 mt-2">
+            <TouchableOpacity
+              className="flex flex-row items-center justify-center p-2 bg-white rounded-xl flex-1"
+              onPress={() => {}}
+            >
+              <Text className="text-dark text-lg font-semibold">Oui !</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="flex flex-row items-center justify-center p-2 bg-white rounded-xl flex-1"
+              onPress={() => {}}
+            >
+              <Text className="text-dark text-lg font-semibold">
+                Pas encore
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View className="flex flex-row items-center w-full p-4 border-2 rounded-xl border-lightGray mt-6">
+          <Text className="font-medium text-lg">Rappel activé</Text>
+          <Switch
+            className="ml-auto"
+            trackColor={{ false: '#EFF1F6', true: '#83F9D6' }}
+            thumbColor="#ffffff"
+            ios_backgroundColor="#EFF1F6"
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+          />
+        </View>
         <View className="flex-1" />
         <TouchableOpacity
-          className="flex flex-row gap-2 items-center justify-center p-4 bg-main rounded-xl w-full"
-          onPress={sendMessage}
+          className="flex flex-row items-center justify-center p-4 bg-white rounded-xl w-full"
+          onPress={() => {}}
         >
-          <Text className="text-dark font-semibold">Désactiver le rappel</Text>
+          <Text className="text-red-400 text-xl font-semibold">Supprimer</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
