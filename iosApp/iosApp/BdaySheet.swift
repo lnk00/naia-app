7cd856578ceed0feeace89157ac43ddd2e4b771e
Damien Dumontet <damien.dumontet@protonmail.com>
2024-04-23 15:37:38 -0300

    Add gift alert

--- iosApp/iosApp/BdaySheet.swift
+++ iosApp/iosApp/BdaySheet.swift
@@ -5,6 +5,8 @@ struct BdaySheet: View {
     var bday: Birthday
     var onDelete: (_ id: KbsonBsonObjectId) -> Void
     @Environment(\.presentationMode) var presentationMode
+    @State private var showGiftAlert: Bool = false
+    @State private var giftIdea: String = ""
 
     func formatDate(date: String) -> String {
         let formatter = DateFormatter()
@@ -17,9 +19,53 @@ struct BdaySheet: View {
 
     var body: some View {
         VStack {
+            if showGiftAlert {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Une idée cadeau ?").font(.system(size: 20, weight: .bold)).padding(.bottom, 2)
+                        Spacer()
+                    }
+                    HStack {
+                        Text("Aidez nous à vous proposer les meilleurs cadeaux dans notre prochaine mise a jour.").font(.system(size: 16, weight: .medium)).padding(.bottom, 4)
+                        Spacer()
+                    }
+
+                    TextField("", text: $giftIdea, prompt: Text("Nike air force 1 édition limité").foregroundColor(Color(hex: 0xF0EFF0)), axis: .vertical)
+                        .padding()
+                        .textFieldStyle(.plain)
+                        .font(.system(size: 16, weight: .semibold))
+                        .lineLimit(3, reservesSpace: true)
+                        .foregroundColor(Color("AccentGreen"))
+                        .background(RoundedRectangle(cornerRadius: 8).fill(.white))
+                        .accentColor(Color("AccentRed"))
+                        .autocorrectionDisabled()
+                    HStack {
+                        Button(action: {}) {
+                            Text("envoyer")
+                                .font(.system(size: 20, weight: .bold))
+                                .frame(maxWidth: 140, maxHeight: 40)
+                                .foregroundColor(Color.white)
+                                .background(Color("AccentGreen"))
+                                .clipShape(RoundedRectangle(cornerRadius: 70))
+                        }
+                        Spacer()
+                    }
+
+                }
+                .ignoresSafeArea()
+                .frame(width: UIScreen.main.bounds.width * 0.85)
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 12).fill(Color(hex: 0xF0EFF0)))
+                .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .trailing)))
+                .padding()
+            }
+            Spacer()
             Image(bday.img).resizable().frame(width: 120, height: 120).aspectRatio(contentMode: .fit).padding(.bottom, 8)
-            Text("\(bday.firstname) \(bday.lastname)").font(.system(size: 22, weight: .black)).padding(.bottom, 2)
-            Text("\(formatDate(date: bday.date))").font(.system(size: 20, weight: .black)).foregroundColor(Color("AccentGreen")).padding(.bottom, 8)
+            VStack {
+                Text("\(bday.firstname) \(bday.lastname)").font(.system(size: 22, weight: .black)).padding(.bottom, 2)
+                Text("\(formatDate(date: bday.date))").font(.system(size: 20, weight: .black)).foregroundColor(Color("AccentGreen")).padding(.bottom, 8)
+            }
+
             HStack {
                 Button(action: {}) {
                     Image(systemName: "bell.badge")
@@ -41,8 +87,11 @@ struct BdaySheet: View {
                 .disabled(false)
 
             }
+            Spacer()
         }
         .presentationDetents([.large])
         .presentationDragIndicator(.visible)
+        .ignoresSafeArea(.keyboard)
+
     }
 }
