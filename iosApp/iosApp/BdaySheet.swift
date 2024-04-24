05321d863290e62c4e23aec6a9d44af7a910ba94
Damien Dumontet <damien.dumontet@protonmail.com>
2024-04-24 08:52:39 -0300

    Fix gift idea behavior

--- iosApp/iosApp/BdaySheet.swift
+++ iosApp/iosApp/BdaySheet.swift
@@ -1,11 +1,17 @@
 import SwiftUI
 import Shared
 
+extension View {
+    func endEditing() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
 struct BdaySheet: View {
     var bday: Birthday
     var onDelete: (_ id: KbsonBsonObjectId) -> Void
     @Environment(\.presentationMode) var presentationMode
-    @State private var showGiftAlert: Bool = false
+    @State private var showGiftAlert: Bool = true
     @State private var giftIdea: String = ""
 
     func formatDate(date: String) -> String {
@@ -19,14 +25,48 @@ struct BdaySheet: View {
 
     var body: some View {
         VStack {
+
+
+            VStack {
+                Spacer()
+                Image(bday.img).resizable().frame(width: 120, height: 120).aspectRatio(contentMode: .fit).padding(.bottom, 8)
+                VStack {
+                    Text("\(bday.firstname) \(bday.lastname)").font(.system(size: 22, weight: .black)).padding(.bottom, 2)
+                    Text("\(formatDate(date: bday.date))").font(.system(size: 20, weight: .black)).foregroundColor(Color("AccentGreen")).padding(.bottom, 8)
+                }
+
+                HStack {
+                    Button(action: {}) {
+                        Image(systemName: "bell.badge")
+                            .font(.system(size: 20, weight: .bold))
+                            .frame(width: 70, height: 70)
+                            .foregroundColor(Color.white)
+                            .background(Color("AccentGreen"))
+                            .clipShape(Circle())
+                    }
+                    .disabled(false)
+                    Button(action: { onDelete(bday.id); deleteScheduledNotification(bday.id); presentationMode.wrappedValue.dismiss() }) {
+                        Image(systemName: "trash")
+                            .font(.system(size: 20, weight: .bold))
+                            .frame(width: 70, height: 70)
+                            .foregroundColor(Color.white)
+                            .background(Color("AccentRed"))
+                            .clipShape(Circle())
+                    }
+                    .disabled(false)
+
+                }
+                Spacer()
+            }
+
             if showGiftAlert {
                 VStack(alignment: .leading) {
                     HStack {
-                        Text("Une idée cadeau ?").font(.system(size: 20, weight: .bold)).padding(.bottom, 2)
+                        Text("Une idée cadeau ?").font(.system(size: 18, weight: .semibold)).padding(.bottom, 2)
                         Spacer()
                     }
                     HStack {
-                        Text("Aidez nous à vous proposer les meilleurs cadeaux dans notre prochaine mise a jour.").font(.system(size: 16, weight: .medium)).padding(.bottom, 4)
+                        Text("Aidez nous à vous proposer les meilleurs cadeaux dans notre prochaine mise a jour.").font(.system(size: 14, weight: .regular)).padding(.bottom, 4).fixedSize(horizontal: false, vertical: true)
                         Spacer()
                     }
 
@@ -37,61 +77,47 @@ struct BdaySheet: View {
                         .lineLimit(3, reservesSpace: true)
                         .foregroundColor(Color("AccentGreen"))
                         .background(RoundedRectangle(cornerRadius: 8).fill(.white))
-                        .accentColor(Color("AccentRed"))
+                        .accentColor(Color("AccentGreen"))
                         .autocorrectionDisabled()
+                        .padding(.bottom, 8)
+                        .submitLabel(.send)
+
+
                     HStack {
                         Button(action: {}) {
                             Text("envoyer")
-                                .font(.system(size: 20, weight: .bold))
-                                .frame(maxWidth: 140, maxHeight: 40)
+                                .font(.system(size: 18, weight: .semibold))
+                                .frame(maxWidth: .infinity, minHeight: 40, maxHeight: 40)
                                 .foregroundColor(Color.white)
                                 .background(Color("AccentGreen"))
                                 .clipShape(RoundedRectangle(cornerRadius: 70))
                         }
-                        Spacer()
+                        Button(action: {
+                            withAnimation {
+                                showGiftAlert.toggle()
+                            }
+                        }) {
+                            Text("fermer")
+                                .font(.system(size: 18, weight: .semibold))
+                                .frame(maxWidth: .infinity, minHeight: 40, maxHeight: 40)
+                                .foregroundColor(Color.white)
+                                .background(Color("AccentRed"))
+                                .clipShape(RoundedRectangle(cornerRadius: 70))
+                        }
                     }
 
                 }
-                .ignoresSafeArea()
-                .frame(width: UIScreen.main.bounds.width * 0.85)
+                .frame(width: UIScreen.main.bounds.width * 0.8)
                 .padding()
                 .background(RoundedRectangle(cornerRadius: 12).fill(Color(hex: 0xF0EFF0)))
                 .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .trailing)))
-                .padding()
+                .padding(24)
             }
-            Spacer()
-            Image(bday.img).resizable().frame(width: 120, height: 120).aspectRatio(contentMode: .fit).padding(.bottom, 8)
-            VStack {
-                Text("\(bday.firstname) \(bday.lastname)").font(.system(size: 22, weight: .black)).padding(.bottom, 2)
-                Text("\(formatDate(date: bday.date))").font(.system(size: 20, weight: .black)).foregroundColor(Color("AccentGreen")).padding(.bottom, 8)
-            }
-
-            HStack {
-                Button(action: {}) {
-                    Image(systemName: "bell.badge")
-                        .font(.system(size: 20, weight: .bold))
-                        .frame(width: 70, height: 70)
-                        .foregroundColor(Color.white)
-                        .background(Color("AccentGreen"))
-                        .clipShape(Circle())
-                }
-                .disabled(false)
-                Button(action: { onDelete(bday.id); deleteScheduledNotification(bday.id); presentationMode.wrappedValue.dismiss() }) {
-                    Image(systemName: "trash")
-                        .font(.system(size: 20, weight: .bold))
-                        .frame(width: 70, height: 70)
-                        .foregroundColor(Color.white)
-                        .background(Color("AccentRed"))
-                        .clipShape(Circle())
-                }
-                .disabled(false)
-
-            }
-            Spacer()
         }
         .presentationDetents([.large])
         .presentationDragIndicator(.visible)
-        .ignoresSafeArea(.keyboard)
-
+        .onTapGesture {
+            self.endEditing()
+        }
     }
 }
